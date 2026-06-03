f403a7b9a769fe2ab5a1d88dc2213c9ec892ea64
69520 <yan.zhu@bangdao-tech.com>
2026-06-03 11:47:48 +0800

    fix(build): include protobuf well-known types

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apt-get update \
     && apt-get install -y --no-install-recommends \
         ca-certificates \
         cmake \
+        libprotobuf-dev \
         pkg-config \
         protobuf-compiler \
     && rm -rf /var/lib/apt/lists/*
--- doro-protocol/build.rs
+++ doro-protocol/build.rs
@@ -1,4 +1,32 @@
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile_protos(&["proto/doro/agent/v1/agent.proto"], &["proto"])?;
+    println!("cargo:rerun-if-env-changed=PROTOC_INCLUDE");
+
+    let protos = [PathBuf::from("proto/doro/agent/v1/agent.proto")];
+    let mut includes = vec![PathBuf::from("proto")];
+
+    if let Some(protoc_include) = env::var_os("PROTOC_INCLUDE") {
+        includes.push(PathBuf::from(protoc_include));
+    }
+
+    for include in [
+        "/usr/include",
+        "/usr/local/include",
+        "/opt/homebrew/include",
+        "/opt/local/include",
+    ] {
+        if Path::new(include)
+            .join("google/protobuf/timestamp.proto")
+            .exists()
+        {
+            includes.push(PathBuf::from(include));
+        }
+    }
+
+    tonic_build::configure().compile_protos(&protos, &includes)?;
     Ok(())
 }
